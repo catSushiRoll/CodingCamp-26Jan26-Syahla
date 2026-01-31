ba547a3362035f0cb5d203e3753b6d58b0b768c2
Syahla <syahla265@gmail.com>
2026-01-31 10:44:33 +0700

    feat:adding new page for some section

--- js/script.js
+++ js/script.js
@@ -1,16 +1,59 @@
-const welcomeText = document.getElementById("welcome-text");
-const setNameBtn = document.getElementById("set-name-btn");
-
-let storedName = localStorage.getItem("name");
-
-if (storedName) {
-  welcomeText.innerText = `Hi, ${storedName}. Welcome to Our Website`;
+if (!sessionStorage.getItem("username")){
+  welcomeMessage();
 }
 
-setNameBtn.addEventListener("click", () => {
-  let name = prompt("What's your name?");
-  if (name && name.trim() !== "") {
-    localStorage.setItem("name", name);
-    welcomeText.innerText = `Hi, ${name}. Welcome to Our Website`;
+function welcomeMessage(){
+  let userResponse = prompt("Welcome to Starseeker! So, what's your name?");
+  if (userResponse === null || userResponse.trim() === ""){
+    userResponse = "Guest";
   }
-});
+  sessionStorage.setItem("username", userResponse)
+  document.getElementById("welcome-speech").innerText = `Hello, ${userResponse}!`;
+}
+
+document.getElementById("welcome-speech").innerText =`Hello, ${sessionStorage.getItem("username") || "Guest"}!`;
+
+function submitMessage(event){
+  event.preventDefault();
+  
+  const name=document.getElementById('name').value;
+  const email=document.getElementById('email').value;
+  const birthDate = document.getElementById('birth-date').value;
+  const genderInput = document.querySelector('input[name="gender"]:checked');
+    if (!genderInput) {
+        alert('Please select a gender!');
+        return;
+    }
+  const gender = genderInput.value;
+  const message = document.getElementById('message').value;
+
+  const dateObj = new Date(birthDate);
+  const formattedDate = dateObj.toLocaleDateString('id-ID', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+  });
+
+  const now = new Date();
+  const currentTime = now.toLocaleString('id-ID', {
+      weekday: 'short',
+      month: 'short',
+      day: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      timeZone: 'Asia/Jakarta',
+      timeZoneName: 'short'
+  });
+
+  document.getElementById('currentTime').textContent = currentTime;
+  document.getElementById('resultName').textContent = name;
+  document.getElementById('resultEmail').textContent = email;
+  document.getElementById('resultDate').textContent = formattedDate;
+  document.getElementById('resultGender').textContent = gender;
+  document.getElementById('resultMessage').textContent = message;
+
+  const resultContainer = document.getElementById('resultContainer');
+  resultContainer.classList.remove('hidden');
+}
